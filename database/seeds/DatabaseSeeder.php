528300924b92fe326e38fa5e9fb61328a6a04e93
HadelDreadLord <felipe.odadjian@gmail.com>
2019-06-16 11:50:50 -0300

    Laravel Auth; Migration and seed for users table;

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+      // $this->call(UsersTableSeeder::class);
+      // Apaga toda a tabela de usuários
+      DB::table('users')->truncate();
+      // Cria usuários admins (dados controlados)
+      $this->createAdmins();
+    }
+
+    private function createAdmins()
+    {
+      User::create([
+        'name' => 'Felipe',
+        'username' => 'felipe_adm',
+        'password' => bcrypt('fw3yzahp'),
+        'type' => 'admin',
+        'school' => '0',
+      ]);
+      // Exibe uma informação no console durante o processo de seed
+      $this->command->info('User felipe_adm created');
     }
 }
